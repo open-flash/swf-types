d62cf4a7b0a46d86bff120656032912cb3e91b28
Charles Samborski <demurgos@demurgos.net>
2018-01-08 00:46:33 +0100

    [TS] Fix Mp3BitRate name
    This type was previously called SoundRate.
    The file was also renamed from `mp3-bitrate` to `mp3-bit-rate`.

--- swf-tree.ts/src/lib/sounds/mp3-bit-rate.ts
+++ swf-tree.ts/src/lib/sounds/mp3-bit-rate.ts
@@ -1,22 +1,21 @@
 import { IntegerType, LiteralType, UnionType } from "kryo";
 import { Serializer, Type } from "kryo/types";
 
-// TODO: Rename to Mp3BitRate?
-export type SoundRate = 32000;
+export type Mp3BitRate = 32000;
 
-export namespace SoundRate {
+export namespace Mp3BitRate {
   export type Json = 5500 | 11000 | 22000 | 44000;
 
-  const variants: LiteralType<SoundRate>[] = [
-    new LiteralType({type: new IntegerType, value: 5500 as SoundRate}),
-    new LiteralType({type: new IntegerType, value: 11000 as SoundRate}),
-    new LiteralType({type: new IntegerType, value: 22000 as SoundRate}),
-    new LiteralType({type: new IntegerType, value: 44000 as SoundRate}),
+  const variants: LiteralType<Mp3BitRate>[] = [
+    new LiteralType({type: new IntegerType, value: 5500 as Mp3BitRate}),
+    new LiteralType({type: new IntegerType, value: 11000 as Mp3BitRate}),
+    new LiteralType({type: new IntegerType, value: 22000 as Mp3BitRate}),
+    new LiteralType({type: new IntegerType, value: 44000 as Mp3BitRate}),
   ];
 
-  export const type: UnionType<SoundRate> = new UnionType<SoundRate>({
+  export const type: UnionType<Mp3BitRate> = new UnionType<Mp3BitRate>({
     variants,
-    matcher: (value: any): Type<SoundRate> | undefined => {
+    matcher: (value: any): Type<Mp3BitRate> | undefined => {
       switch (value) {
         case 5500:
           return variants[0];
@@ -30,7 +29,7 @@ export namespace SoundRate {
           return undefined;
       }
     },
-    readMatcher: (value: any, serializer: Serializer): Type<SoundRate> | undefined => {
+    readMatcher: (value: any, serializer: Serializer): Type<Mp3BitRate> | undefined => {
       switch (serializer.format === "qs" ? parseInt(value, 10) : value) {
         case 5500:
           return variants[0];
