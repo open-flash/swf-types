aae73199e43fe38755bdf5da6c507ad400254d23
Charles Samborski <demurgos@demurgos.net>
2019-04-22 13:36:22 +0200

    [rs] Split bin dependencies from lib
    This commit moves the binary only dependencies to the `bin` sub-crate using workspaces. The main difference is that the lib no longer depends on `serde_json`. This is a first step into making serialization a feature.

---
+++ rs/bin/src/main.rs
@@ -0,0 +1,16 @@
+use swf_tree::Movie;
+
+fn main() {
+  let args: Vec<String> = std::env::args().collect();
+  if args.len() < 2 {
+    eprintln!("Missing input path");
+    return;
+  }
+
+  let file_path = &args[1];
+  let json_file = ::std::fs::File::open(file_path).expect("Failed to open file");
+  let reader = ::std::io::BufReader::new(json_file);
+  let movie: Movie = serde_json::from_reader(reader).expect("Failed to read value");
+
+  println!("{}", serde_json::to_string_pretty(&movie).expect("Failed to print value"));
+}
